981bf8c8a76147072b7d9ae58b2841f94cdc4564
traveler20 <60683849+traveler20@users.noreply.github.com>
2021-04-20 20:40:04 +0900

    Add files via upload

--- js/fv.js
+++ js/fv.js
@@ -1,4 +1,9 @@
 // scale-animation
 window.onload = function () {
+	const spinner = document.getElementById("loading");
+
+	// Add .loaded to .loading
+	spinner.classList.add("loaded");
+
 	document.getElementById("header").classList.add("js-active");
 };
