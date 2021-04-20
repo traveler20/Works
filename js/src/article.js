eb1f65d2ed0f84b8906bdd9f72eb0a5442f905e1
traveler20 <60683849+traveler20@users.noreply.github.com>
2021-04-20 10:42:01 +0900

    Add files via upload

--- js/src/article.js
+++ js/src/article.js
@@ -1,4 +1,9 @@
 const others =
-	"<div class='inner'><div class='section-heading'><h2 class='heading-primary'>Others</h2></div><p class='section-lead'>その他の作品群です。</p><div class='others-item'><a href='../printpac/'><img src='../img/thumb/thumb_dummy.png' alt='' /></a><a href='../howmuch/'><img src='../img/thumb/thumb_dummy.png' alt='' /></a><a href='../hardcover/'><img src='../img/thumb/thumb_dummy.png' alt='' /></a></div></div><p class='section-lead' style='margin-bottom: 0px'><a href='../'>その他の実務経験一覧はこちら</a></p>";
+	"<div class='inner'><div class='section-heading'><h2 class='heading-primary'>Others</h2></div><p class='section-lead'>その他の作品群です。</p><div class='others-item'><a href='../printpac/'><img src='../../public/pac_printpac3.png' alt='' /></a><a href='../howmuch/'><img src='../../public/pac_howmuch2.png' alt='' /></a><a href='../comicpac/'><img src='../../public/pac_comicpac2.png' alt='' /></a></div></div><p class='section-lead' style='margin-bottom: 0px'><a href='../'>その他の実務経験一覧はこちら</a></p>";
+
+const returntop =
+	"<p class='section-lead' style='margin-bottom: 50px'><a href='../../' class='password-btn'>Return to Top</a></p>";
 
 document.getElementById("others").innerHTML = others;
+
+document.getElementById("returntop").innerHTML = returntop;
--- js/src/footer.js
+++ js/src/footer.js
@@ -1,6 +1,3 @@
-const returntop =
-	"<p class='section-lead' style='margin-bottom: 50px'><a href='../../' class='password-btn'>Return to Top</a></p>";
-
 const footer =
 	"<footer class='footer'><a href='#' id='js-page-top' class='page-top'>▲</a><div class='inner'><p class='copyright'>Copyright&copy;traveler20. All Rights Reserved.</p></div></footer>";
 
