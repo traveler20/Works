8748c590cd768d37b1ba5bdd55097df3cdedb84b
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2021-11-09 16:59:22 +0900

    🎉renewal 1st

--- asset/js/fv.js
+++
@@ -1,9 +0,0 @@
-// scale-animation
-window.onload = function () {
-	const spinner = document.getElementById("loading");
-
-	// Add .loaded to .loading
-	spinner.classList.add("loaded");
-
-	document.getElementById("header").classList.add("js-active");
-};
