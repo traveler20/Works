9b84d2d77101e563040c476bd3130dc71663ded1
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2021-11-16 08:25:06 +0900

    🎨Add LoadingAnimation

---
+++ docs/asset/js/script.js
@@ -0,0 +1,1 @@
+window.onload=function(){const d=document.getElementById("js-loading");d.classList.add("is-loaded"),document.getElementById("js-header").classList.add("is-show")};
---
+++ src/asset/js/script.js
@@ -0,0 +1,8 @@
+// loading animation
+window.onload = function () {
+	const loadingAnimation = document.getElementById("js-loading");
+
+	loadingAnimation.classList.add("is-loaded");
+
+	document.getElementById("js-header").classList.add("is-show");
+};
