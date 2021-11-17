905e61093afb5b5679e579e6b32c7fbf7b4ca914
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2021-11-18 08:34:09 +0900

    🔧Add Modal Function

--- src/asset/js/script.js
+++ src/asset/js/script.js
@@ -6,3 +6,29 @@ window.onload = function () {
 
 	document.getElementById("js-header").classList.add("is-show");
 };
+
+const modalWrapOpen = function (e) {
+	const dataModalOpen = e.currentTarget.dataset.modalOpen;
+	Array.from(document.querySelectorAll(".p-work__modal")).forEach((e, i) => {
+		if (e.getAttribute("data-modal") === dataModalOpen) {
+			e.classList.toggle("is_open");
+		}
+	});
+};
+
+Array.from(document.querySelectorAll(".p-work__figure")).forEach(
+	(modalOpenElement) => {
+		modalOpenElement.addEventListener("click", modalWrapOpen);
+	}
+);
+
+const modalCloseAction = function (e) {
+	const targetModal = e.currentTarget.closest(".p-work__modal");
+	targetModal.classList.toggle("is_open");
+};
+
+Array.from(document.querySelectorAll(".p-work__close")).forEach(
+	(modalCloseElement) => {
+		modalCloseElement.addEventListener("click", modalCloseAction);
+	}
+);
